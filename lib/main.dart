b2938926b5a11c8fb739fe429750e963e80aa67f
Mtbabazadeh <m2.babazadh@gmail.com>
2023-09-12 19:05:20 +0430

    first commit, initializing project

---
+++ lib/main.dart
@@ -0,0 +1,211 @@
+import 'package:blogclub/data.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const defultFontFamily = 'Avenir';
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final primaryTextColor = const Color(0xff0D253C);
+    final secondryTextColor = const Color(0xff2D4379);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: TextTheme(
+          subtitle1: TextStyle(
+            fontFamily: defultFontFamily,
+            color: secondryTextColor,
+            fontSize: 18,
+            fontWeight: FontWeight.w200,
+          ),
+          headline6: TextStyle(
+              fontFamily: defultFontFamily,
+              fontWeight: FontWeight.bold,
+              color: primaryTextColor),
+          bodyText2: TextStyle(
+              fontFamily: defultFontFamily,
+              color: secondryTextColor,
+              fontSize: 12),
+          headline4:TextStyle(
+            fontFamily: defultFontFamily,
+            fontSize: 24,
+            color: primaryTextColor,
+            fontWeight: FontWeight.w700,
+          ) 
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    final stories = AppDatabase.stories;
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 16, 32, 0),
+                child: Row(
+                  
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Hi, Jonathan!',
+                      style: themeData.textTheme.subtitle1,
+                    ),
+                    Image.asset(
+                      'assets/img/icons/notification.png',
+                      width: 32,
+                      height: 32,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 0, 0, 16),
+                child: Text(
+                  'Explore today’s',
+                  style: themeData.textTheme.headline4,
+                ),
+              ),
+              _StoryList(stories: stories)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _StoryList extends StatelessWidget {
+  const _StoryList({
+    super.key,
+    required this.stories,
+  });
+
+  final List<StoryData> stories;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: 100,
+      child: ListView.builder(
+          itemCount: stories.length,
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+          itemBuilder: (context, index) {
+            final story = stories[index];
+            return _Story(story: story);
+          }),
+    );
+  }
+}
+
+class _Story extends StatelessWidget {
+  const _Story({
+    super.key,
+    required this.story,
+  });
+
+  final StoryData story;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(4, 2, 4, 0),
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              story.isViewed
+                  ? _ProfileImageViewed()
+                  : _ProfileImageNormal(),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Image.asset(
+                  'assets/img/icons/${story.iconFileName}',
+                  width: 24,
+                  height: 24,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Text(story.name),
+        ],
+      ),
+    );
+  }
+
+  Container _ProfileImageNormal() {
+    return Container(
+      width: 68,
+      height: 68,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24),
+        gradient: const LinearGradient(begin: Alignment.topLeft, colors: [
+          Color(0xff376AED),
+          Color(0xff49B0E2),
+          Color(0xff9CECFB),
+        ]),
+      ),
+      child: Container(
+        margin: const EdgeInsets.all(2),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(22),
+        ),
+        padding: const EdgeInsets.all(5),
+        child: _PorofileImage(),
+      ),
+    );
+  }
+
+  Widget _ProfileImageViewed() {
+    return SizedBox(
+      width: 68,
+      height: 68,
+      child: DottedBorder(
+        borderType: BorderType.RRect,
+        strokeWidth: 2,
+        radius: const Radius.circular(24),
+        color: const Color(0xaa7B8BB2),
+        dashPattern: const [8, 3],
+        padding: const EdgeInsets.all(7),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(24),
+          ),
+          child: _PorofileImage(),
+        ),
+      ),
+    );
+  }
+
+  Widget _PorofileImage() {
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(17),
+        child: Image.asset('assets/img/stories/${story.imageFileName}'));
+  }
+}
